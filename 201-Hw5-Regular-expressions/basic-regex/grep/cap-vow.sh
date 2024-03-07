9eb1ec2638bdc57144a0fae5af9e798f3e4a4484
Rynne Fan <133628123+RynneC@users.noreply.github.com>
2024-03-06 21:57:40 -0500

    Update cap-vow.sh

--- 201-Hw5-Regular-expressions/basic-regex/grep/cap-vow.sh
+++ 201-Hw5-Regular-expressions/basic-regex/grep/cap-vow.sh
@@ -3,6 +3,7 @@
 # cap-vow.sh <path to dictionary file>
 # ^ This specifies that the dictionary file path is the first argument
 #   and the angle brackets specifies that it's a mandatory argument
+# print out words in the dictionary that start with a capital vowel
 # (for the sake of this assignment: a,e,i,o,u are vowels, and y is never a vowel)
 # Like in the dictionary file, each word should be on its own line
 # *This must use only one grep command*
