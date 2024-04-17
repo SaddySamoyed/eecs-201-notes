45e9fba272de137b7bc582891af17f964ce9b796
Rynne Fan <133628123+RynneC@users.noreply.github.com>
2024-04-16 21:29:15 -0400

    2

---
+++ 201-Hw11-Editor/eecs201-basic-editors-main/file3.sh
@@ -0,0 +1,1002 @@
+#!/bin/sh
+# cat /etc/proc/cpuinfo
+# du -sh
+# git rebase -i origin/dev
+# cd /
+# git status
+# git commit --amend
+# ls -l
+# whoami
+# df -Th
+# rev $(ls -l) | lolcat
+# whoami
+# rev $(ls -l) | lolcat
+# du -sh
+# git status
+# git commit --amend
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# cd /
+# df -Th
+# ls -l
+# cd /
+# rev $(ls -l) | lolcat
+# git commit --amend
+# git status
+# cat /etc/proc/cpuinfo
+# ls -l
+# df -Th
+# du -sh
+# git rebase -i origin/dev
+# whoami
+# df -Th
+# git rebase -i origin/dev
+# du -sh
+# ls -l
+# cd /
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# git status
+# git commit --amend
+# whoami
+# git status
+# df -Th
+# git commit --amend
+# cat /etc/proc/cpuinfo
+# du -sh
+# ls -l
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# whoami
+# cd /
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# du -sh
+# whoami
+# df -Th
+# rev $(ls -l) | lolcat
+# git commit --amend
+# ls -l
+# cd /
+# git status
+# rev $(ls -l) | lolcat
+# du -sh
+# git commit --amend
+# cd /
+# df -Th
+# ls -l
+# git status
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# whoami
+# cd /
+# git rebase -i origin/dev
+# whoami
+# df -Th
+# git status
+# du -sh
+# rev $(ls -l) | lolcat
+# ls -l
+# cat /etc/proc/cpuinfo
+# git commit --amend
+# cd /
+# ls -l
+# whoami
+# git commit --amend
+# git status
+# df -Th
+# du -sh
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# rev $(ls -l) | lolcat
+# rev $(ls -l) | lolcat
+# git commit --amend
+# ls -l
+# du -sh
+# whoami
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# df -Th
+# cd /
+# git status
+# git commit --amend
+# git status
+# cat /etc/proc/cpuinfo
+# cd /
+# du -sh
+# ls -l
+# whoami
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# df -Th
+# cd /
+# ls -l
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# git commit --amend
+# git status
+# rev $(ls -l) | lolcat
+# du -sh
+# whoami
+# df -Th
+# du -sh
+# rev $(ls -l) | lolcat
+# ls -l
+# whoami
+# cd /
+# git commit --amend
+# git status
+# df -Th
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# cd /
+# du -sh
+# cat /etc/proc/cpuinfo
+# df -Th
+# git status
+# git rebase -i origin/dev
+# whoami
+# git commit --amend
+# ls -l
+# rev $(ls -l) | lolcat
+# rev $(ls -l) | lolcat
+# df -Th
+# git commit --amend
+# cd /
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# ls -l
+# whoami
+# du -sh
+# git status
+# whoami
+# df -Th
+# cd /
+# cat /etc/proc/cpuinfo
+# ls -l
+# git rebase -i origin/dev
+# git status
+# git commit --amend
+# du -sh
+# rev $(ls -l) | lolcat
+# df -Th
+# cd /
+# whoami
+# cat /etc/proc/cpuinfo
+# git status
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# du -sh
+# ls -l
+# git commit --amend
+# whoami
+# git rebase -i origin/dev
+# du -sh
+# git status
+# git commit --amend
+# cd /
+# ls -l
+# cat /etc/proc/cpuinfo
+# df -Th
+# rev $(ls -l) | lolcat
+# whoami
+# git commit --amend
+# du -sh
+# df -Th
+# git status
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# ls -l
+# rev $(ls -l) | lolcat
+# cd /
+# git rebase -i origin/dev
+# git status
+# df -Th
+# whoami
+# rev $(ls -l) | lolcat
+# cd /
+# du -sh
+# cat /etc/proc/cpuinfo
+# ls -l
+# git commit --amend
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# df -Th
+# rev $(ls -l) | lolcat
+# ls -l
+# git commit --amend
+# cd /
+# whoami
+# git status
+# du -sh
+# du -sh
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# cd /
+# git status
+# cat /etc/proc/cpuinfo
+# df -Th
+# ls -l
+# git commit --amend
+# whoami
+# du -sh
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# ls -l
+# git status
+# cd /
+# whoami
+# git commit --amend
+# df -Th
+# git commit --amend
+# df -Th
+# cat /etc/proc/cpuinfo
+# git status
+# rev $(ls -l) | lolcat
+# du -sh
+# whoami
+# ls -l
+# cd /
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# du -sh
+# git status
+# cd /
+# df -Th
+# git rebase -i origin/dev
+# git commit --amend
+# whoami
+# ls -l
+# whoami
+# ls -l
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# cd /
+# git commit --amend
+# du -sh
+# rev $(ls -l) | lolcat
+# git status
+# df -Th
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# git commit --amend
+# whoami
+# du -sh
+# git status
+# ls -l
+# cd /
+# cat /etc/proc/cpuinfo
+# df -Th
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# git commit --amend
+# git status
+# du -sh
+# ls -l
+# cd /
+# whoami
+# df -Th
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# git commit --amend
+# git status
+# rev $(ls -l) | lolcat
+# whoami
+# df -Th
+# cd /
+# ls -l
+# du -sh
+# df -Th
+# git commit --amend
+# cat /etc/proc/cpuinfo
+# whoami
+# git rebase -i origin/dev
+# ls -l
+# rev $(ls -l) | lolcat
+# cd /
+# git status
+# du -sh
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# ls -l
+# git status
+# git commit --amend
+# du -sh
+# whoami
+# df -Th
+# rev $(ls -l) | lolcat
+# cd /
+# whoami
+# git commit --amend
+# git rebase -i origin/dev
+# ls -l
+# du -sh
+# cd /
+# git status
+# df -Th
+# cat /etc/proc/cpuinfo
+# rev $(ls -l) | lolcat
+# cd /
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# du -sh
+# git status
+# git commit --amend
+# df -Th
+# cat /etc/proc/cpuinfo
+# whoami
+# ls -l
+# du -sh
+# cd /
+# ls -l
+# rev $(ls -l) | lolcat
+# git status
+# cat /etc/proc/cpuinfo
+# df -Th
+# whoami
+# git commit --amend
+# git rebase -i origin/dev
+# cd /
+# rev $(ls -l) | lolcat
+# df -Th
+# git rebase -i origin/dev
+# ls -l
+# whoami
+# git status
+# cat /etc/proc/cpuinfo
+# git commit --amend
+# du -sh
+# df -Th
+# du -sh
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# whoami
+# git commit --amend
+# ls -l
+# cd /
+# rev $(ls -l) | lolcat
+# git status
+# cat /etc/proc/cpuinfo
+# cd /
+# df -Th
+# rev $(ls -l) | lolcat
+# git status
+# whoami
+# git rebase -i origin/dev
+# du -sh
+# git commit --amend
+# ls -l
+# whoami
+# cat /etc/proc/cpuinfo
+# git status
+# du -sh
+# ls -l
+# git rebase -i origin/dev
+# cd /
+# git commit --amend
+# df -Th
+# rev $(ls -l) | lolcat
+# whoami
+# git status
+# ls -l
+# cd /
+# cat /etc/proc/cpuinfo
+# df -Th
+# du -sh
+# git commit --amend
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# du -sh
+# git rebase -i origin/dev
+# whoami
+# df -Th
+# git commit --amend
+# cd /
+# git status
+# ls -l
+# cat /etc/proc/cpuinfo
+# rev $(ls -l) | lolcat
+# git commit --amend
+# df -Th
+# whoami
+# rev $(ls -l) | lolcat
+# du -sh
+# cd /
+# ls -l
+# git status
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# du -sh
+# git rebase -i origin/dev
+# cd /
+# whoami
+# rev $(ls -l) | lolcat
+# df -Th
+# cat /etc/proc/cpuinfo
+# git status
+# ls -l
+# git commit --amend
+# df -Th
+# ls -l
+# cat /etc/proc/cpuinfo
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# du -sh
+# git status
+# cd /
+# git commit --amend
+# whoami
+# ls -l
+# git rebase -i origin/dev
+# git commit --amend
+# df -Th
+# cd /
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# git status
+# du -sh
+# whoami
+# rev $(ls -l) | lolcat
+# ls -l
+# git commit --amend
+# cd /
+# du -sh
+# git rebase -i origin/dev
+# df -Th
+# whoami
+# cat /etc/proc/cpuinfo
+# git status
+# cd /
+# rev $(ls -l) | lolcat
+# whoami
+# df -Th
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# ls -l
+# du -sh
+# git commit --amend
+# git status
+# git commit --amend
+# du -sh
+# git rebase -i origin/dev
+# git status
+# ls -l
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# whoami
+# df -Th
+# cd /
+# df -Th
+# cd /
+# cat /etc/proc/cpuinfo
+# git status
+# ls -l
+# git commit --amend
+# du -sh
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# whoami
+# git status
+# df -Th
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# cd /
+# git commit --amend
+# whoami
+# ls -l
+# du -sh
+# df -Th
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# cd /
+# git status
+# whoami
+# git rebase -i origin/dev
+# ls -l
+# du -sh
+# git commit --amend
+# git rebase -i origin/dev
+# df -Th
+# whoami
+# du -sh
+# cat /etc/proc/cpuinfo
+# git status
+# git commit --amend
+# ls -l
+# cd /
+# rev $(ls -l) | lolcat
+# git status
+# du -sh
+# cd /
+# whoami
+# git rebase -i origin/dev
+# git commit --amend
+# ls -l
+# cat /etc/proc/cpuinfo
+# df -Th
+# rev $(ls -l) | lolcat
+# whoami
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# git commit --amend
+# ls -l
+# rev $(ls -l) | lolcat
+# du -sh
+# cd /
+# git status
+# df -Th
+# ls -l
+# df -Th
+# whoami
+# du -sh
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# cd /
+# git commit --amend
+# cat /etc/proc/cpuinfo
+# git status
+# ls -l
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# git status
+# cat /etc/proc/cpuinfo
+# whoami
+# df -Th
+# du -sh
+# cd /
+# git commit --amend
+# du -sh
+# cat /etc/proc/cpuinfo
+# git commit --amend
+# ls -l
+# git rebase -i origin/dev
+# whoami
+# rev $(ls -l) | lolcat
+# df -Th
+# cd /
+# git status
+# git status
+# du -sh
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# cd /
+# df -Th
+# git commit --amend
+# ls -l
+# whoami
+# du -sh
+# git commit --amend
+# ls -l
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# df -Th
+# whoami
+# cd /
+# git status
+# cat /etc/proc/cpuinfo
+# whoami
+# git commit --amend
+# git status
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# cd /
+# du -sh
+# df -Th
+# rev $(ls -l) | lolcat
+# ls -l
+# df -Th
+# whoami
+# cd /
+# ls -l
+# git rebase -i origin/dev
+# git commit --amend
+# git status
+# rev $(ls -l) | lolcat
+# du -sh
+# cat /etc/proc/cpuinfo
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# cd /
+# git commit --amend
+# df -Th
+# du -sh
+# whoami
+# ls -l
+# git status
+# rev $(ls -l) | lolcat
+# df -Th
+# cd /
+# whoami
+# git status
+# du -sh
+# git commit --amend
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# ls -l
+# du -sh
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# whoami
+# ls -l
+# git status
+# git commit --amend
+# df -Th
+# cd /
+# cat /etc/proc/cpuinfo
+# git status
+# whoami
+# cat /etc/proc/cpuinfo
+# cd /
+# du -sh
+# ls -l
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# git commit --amend
+# df -Th
+# du -sh
+# ls -l
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# cd /
+# df -Th
+# whoami
+# git status
+# git commit --amend
+# rev $(ls -l) | lolcat
+# git commit --amend
+# df -Th
+# du -sh
+# git status
+# rev $(ls -l) | lolcat
+# ls -l
+# git rebase -i origin/dev
+# whoami
+# cat /etc/proc/cpuinfo
+# cd /
+# cat /etc/proc/cpuinfo
+# rev $(ls -l) | lolcat
+# git commit --amend
+# df -Th
+# du -sh
+# git rebase -i origin/dev
+# cd /
+# ls -l
+# whoami
+# git status
+# cd /
+# du -sh
+# ls -l
+# git commit --amend
+# git status
+# rev $(ls -l) | lolcat
+# whoami
+# df -Th
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# df -Th
+# git commit --amend
+# whoami
+# rev $(ls -l) | lolcat
+# ls -l
+# du -sh
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# git status
+# cd /
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# cd /
+# whoami
+# ls -l
+# df -Th
+# git status
+# du -sh
+# git commit --amend
+# whoami
+# df -Th
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# git commit --amend
+# du -sh
+# ls -l
+# git rebase -i origin/dev
+# cd /
+# git status
+# whoami
+# df -Th
+# git rebase -i origin/dev
+# du -sh
+# git status
+# git commit --amend
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# cd /
+# ls -l
+# df -Th
+# cd /
+# du -sh
+# rev $(ls -l) | lolcat
+# git commit --amend
+# ls -l
+# cat /etc/proc/cpuinfo
+# whoami
+# git status
+# git rebase -i origin/dev
+# git rebase -i origin/dev
+# du -sh
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# whoami
+# git commit --amend
+# ls -l
+# cd /
+# df -Th
+# git status
+# git rebase -i origin/dev
+# df -Th
+# ls -l
+# du -sh
+# rev $(ls -l) | lolcat
+# git status
+# whoami
+# cd /
+# cat /etc/proc/cpuinfo
+# git commit --amend
+# git commit --amend
+# ls -l
+# whoami
+# df -Th
+# du -sh
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# cd /
+# git status
+# git rebase -i origin/dev
+# whoami
+# ls -l
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# df -Th
+# cd /
+# git status
+# du -sh
+# git commit --amend
+# rev $(ls -l) | lolcat
+# du -sh
+# df -Th
+# ls -l
+# whoami
+# rev $(ls -l) | lolcat
+# git commit --amend
+# cat /etc/proc/cpuinfo
+# git status
+# cd /
+# git rebase -i origin/dev
+# git commit --amend
+# git rebase -i origin/dev
+# ls -l
+# rev $(ls -l) | lolcat
+# du -sh
+# df -Th
+# git status
+# cd /
+# whoami
+# cat /etc/proc/cpuinfo
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# df -Th
+# ls -l
+# cd /
+# whoami
+# git status
+# git commit --amend
+# du -sh
+# cat /etc/proc/cpuinfo
+# git status
+# whoami
+# df -Th
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# ls -l
+# cd /
+# git commit --amend
+# du -sh
+# rev $(ls -l) | lolcat
+# df -Th
+# git rebase -i origin/dev
+# git status
+# git commit --amend
+# cd /
+# ls -l
+# du -sh
+# whoami
+# cat /etc/proc/cpuinfo
+# git commit --amend
+# git rebase -i origin/dev
+# whoami
+# cd /
+# ls -l
+# cat /etc/proc/cpuinfo
+# df -Th
+# du -sh
+# git status
+# rev $(ls -l) | lolcat
+# cd /
+# rev $(ls -l) | lolcat
+# whoami
+# git rebase -i origin/dev
+# ls -l
+# git status
+# cat /etc/proc/cpuinfo
+# du -sh
+# df -Th
+# git commit --amend
+# git commit --amend
+# git rebase -i origin/dev
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# git status
+# whoami
+# cd /
+# du -sh
+# ls -l
+# df -Th
+# git status
+# du -sh
+# cd /
+# git commit --amend
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# whoami
+# rev $(ls -l) | lolcat
+# ls -l
+# df -Th
+# df -Th
+# git rebase -i origin/dev
+# whoami
+# rev $(ls -l) | lolcat
+# git status
+# cat /etc/proc/cpuinfo
+# du -sh
+# cd /
+# ls -l
+# git commit --amend
+# df -Th
+# git commit --amend
+# cd /
+# du -sh
+# whoami
+# git rebase -i origin/dev
+# git status
+# cat /etc/proc/cpuinfo
+# rev $(ls -l) | lolcat
+# ls -l
+# git commit --amend
+# git status
+# git rebase -i origin/dev
+# ls -l
+# cat /etc/proc/cpuinfo
+# du -sh
+# rev $(ls -l) | lolcat
+# whoami
+# df -Th
+# cd /
+# ls -l
+# git status
+# du -sh
+# df -Th
+# rev $(ls -l) | lolcat
+# git rebase -i origin/dev
+# cd /
+# cat /etc/proc/cpuinfo
+# git commit --amend
+# whoami
+# git commit --amend
+# ls -l
+# du -sh
+# df -Th
+# git rebase -i origin/dev
+# git status
+# cd /
+# cat /etc/proc/cpuinfo
+# rev $(ls -l) | lolcat
+# whoami
+# ls -l
+# cat /etc/proc/cpuinfo
+# git status
+# git commit --amend
+# rev $(ls -l) | lolcat
+# du -sh
+# cd /
+# whoami
+# git rebase -i origin/dev
+# df -Th
+# git status
+# cd /
+# whoami
+# du -sh
+# rev $(ls -l) | lolcat
+# git commit --amend
+# cat /etc/proc/cpuinfo
+# git rebase -i origin/dev
+# ls -l
+# df -Th
+# ls -l
+# rev $(ls -l) | lolcat
+# cd /
+# git status
+# cat /etc/proc/cpuinfo
+# df -Th
+# git commit --amend
+# git rebase -i origin/dev
+# du -sh
+# whoami
+# git status
+# whoami
+# df -Th
+# rev $(ls -l) | lolcat
+# cd /
+# git commit --amend
+# du -sh
+# cat /etc/proc/cpuinfo
+# ls -l
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# du -sh
+# cd /
+# ls -l
+# git rebase -i origin/dev
+# whoami
+# df -Th
+# git commit --amend
+# git status
+# rev $(ls -l) | lolcat
+# whoami
+# du -sh
+# df -Th
+# git commit --amend
+# git status
+# git rebase -i origin/dev
+# cd /
+# ls -l
+# rev $(ls -l) | lolcat
+# cat /etc/proc/cpuinfo
+# df -Th
+# du -sh
+# git rebase -i origin/dev
+# cat /etc/proc/cpuinfo
+# git commit --amend
+# rev $(ls -l) | lolcat
+# whoami
+# git status
+# cd /
+# ls -l
+# git status
+# du -sh
+# whoami
+# df -Th
+# git rebase -i origin/dev
+# cd /
+# git commit --amend
+# cat /etc/proc/cpuinfo
+# ls -l
+# rev $(ls -l) | lolcat
+# ls -l
+# git commit --amend
+# whoami
+# cat /etc/proc/cpuinfo
+# df -Th
+# git status
+# rev $(ls -l) | lolcat
+# cd /
+# git rebase -i origin/dev
+# du -sh
+echo "hello world"
